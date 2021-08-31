e26268d10389273785712a9dac6caab6e199a624
bobby6381 <bobby6381@gmail.com>
2021-08-31 15:28:14 -0500

    Initial output via javascript was succesfull

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,23 @@
 // Assignment code here
-
+function generatePassword() {
+  return "$523CATKLSJDFKLSJ"
+}
+// var myName = {
+//   first: "bobby", 
+//   last: "gallagher",
+//   isDriving: true
+// };
+// console.log(myName.last)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+console.log("DATA",passwordText)
   passwordText.value = password;
 
 }
